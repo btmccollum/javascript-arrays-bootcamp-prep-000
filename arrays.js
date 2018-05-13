71ad80a42165dcd50d1e9e209e682f5677a00d0a
Brad McCollum <btmcco8@gmail.com>
2018-05-13 19:38:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,17 +1,2 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array, element){
-  Array = [`${element}, ...chocolateBars`]
-}
-function destructivelyAddElementToBeginningOfArray(array, element){
-chocolateBars.unshift(`${element}`);
-}
-function addElementToEndOfArray(){
-chocolateBars = [...chocolateBars, "tacos"]
-}
-function destructivelyAddElementToEndOfArray(){
-  chocolateBars.unpush("tacos");
-}
-function accessElementInArray(array, index){
-  console.log(chocolateBars[2]);
-}
