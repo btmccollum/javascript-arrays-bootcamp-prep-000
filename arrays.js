2a9c3e31af67d65085f47906f0f4126e718082ab
Brad McCollum <btmcco8@gmail.com>
2018-05-13 20:36:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,6 +8,6 @@ function addElementToBeginningOfArray(array, element){
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   
-  return array.push(element);
+  return Array.push(element);
   
 }
