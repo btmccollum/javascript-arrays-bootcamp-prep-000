f9b192faecd644488fcf379734f4fa992478c8ee
Brad McCollum <btmcco8@gmail.com>
2018-05-13 19:53:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 var dogBreeds = new Array("shiba", "boston terrier");
 
