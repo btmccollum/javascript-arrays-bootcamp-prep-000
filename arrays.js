71f10e827becb853e88ae59c0886c86d2e561823
Brad McCollum <btmcco8@gmail.com>
2018-05-13 21:08:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,5 +35,9 @@ function accessElementInArray(array, index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  return array.shift();
+  
+  array.shift();
+  
+  return array;
+  
 }
