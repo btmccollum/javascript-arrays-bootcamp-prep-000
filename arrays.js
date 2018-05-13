7ef286aee40c5312a0aaf398016283c7cefce0f7
Brad McCollum <btmcco8@gmail.com>
2018-05-13 19:51:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 var dogBreeds = new Array("shiba", "boston terrier");
+
+function addElementToBeginningOfArray(array, element){
+  
+  var dogBreeds = ["shiba inu", "dalmation"]
+  
+  dogBreeds.unshift(${element})
+}
